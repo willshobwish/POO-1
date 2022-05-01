7b93ae3afb55f338cd88d68f9a110a06bd4c5db6
Willian <willianyos13@gmail.com>
2022-05-01 00:30:31 -0300

    Creation of exercicio 03

---
+++ Aulas/2022-04-29-Exercicios/Exercicio03/Veiculos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03 {
+    internal class Veiculos {
+        string marca;
+        string modelo;
+        int ano;
+        bool disponivel;
+        public void emprestar() {
+
+        }
+        public void devolver() {
+
+        }
+        public void estaDisponivel() {
+
+        }
+    }
+}
