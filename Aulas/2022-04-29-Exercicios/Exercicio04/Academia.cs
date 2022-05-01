631dab5d22a1d6ec5e63458ef3ec14110c6ca11e
Willian <willianyos13@gmail.com>
2022-05-01 00:30:43 -0300

    Creation of exercicio 04

---
+++ Aulas/2022-04-29-Exercicios/Exercicio04/Academia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio04 {
+    internal class Academia {
+        string nome;
+        int idade;
+        double peso;
+        double altura;
+        public bool isMenor() {
+            if (this.idade < 18) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        public void setDados(string nome, int idade, double peso, double altura) {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+            this.altura = altura;
+        }
+        public double imc() {
+            double calculoImc = peso / (Math.Pow(altura, 2));
+            return calculoImc;
+        }
+    }
+}
---
+++ Aulas/2022-04-29-Exercicios/Exercicio04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio04 {
+    internal class Program {
+        static void Main(string[] args) {
+            Academia Aluno = new Academia();
+            Aluno.setDados("Teste", 17, 87, 189);
+            Console.WriteLine(Aluno.imc());
+            Console.ReadLine();
+        }
+    }
+}
