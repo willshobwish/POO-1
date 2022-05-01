91952553717ed8389a5fa7f383a0ea365072a44b
Willian <willianyos13@gmail.com>
2022-05-01 00:30:18 -0300

    Creation of exercicio 02

---
+++ Aulas/2022-04-29-Exercicios/Exercicio02/Televisao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02 {
+    internal class Televisao {
+        int canal;
+        int volume;
+        bool ligada;
+        public string getStatusTelevisao() {
+            string status = $@"Canal: {canal}
+Volume: {volume}
+Ligada: {ligada}";
+            return status;
+        }
+        public void aumentarCanal() {
+            this.canal += 1;
+        }
+        public void diminuirCanal() {
+            this.canal -= 1;
+
+        }
+        public void aumentarVolume() {
+            this.volume += 1;
+        }
+        public void diminuirVolume() {
+            this.volume -= 1;
+        }
+        public void desligar() {
+            this.ligada = false;
+        }
+        public void ligar() {
+            this.ligada = true;
+        }
+        public void SetCanal(int canal) {
+            this.canal = canal;
+        }
+    }
+}
