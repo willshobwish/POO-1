79db02213b0006862edf781b7e0df6d832b56790
Willian <willianyos13@gmail.com>
2022-05-01 00:42:21 -0300

    Added demonstração

---
+++ Aulas/2022-04-27-Demonstração/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_04_27___Demonstração {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+    class name {
+
+    }
+}
