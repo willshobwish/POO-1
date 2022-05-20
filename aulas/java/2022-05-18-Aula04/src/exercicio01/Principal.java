7e5b4113737fc741044cf4eb827f39208b5da91c
Willian <willianyos13@gmail.com>
2022-05-19 21:51:19 -0300

    Created aula 04 files

---
+++ aulas/java/2022-05-18-Aula04/src/exercicio01/Principal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio01;
+
+public class Principal{
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.test();
+        System.out.println("Teste");
+    }
+}
