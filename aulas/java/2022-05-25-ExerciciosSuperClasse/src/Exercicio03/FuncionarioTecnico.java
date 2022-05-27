7727bbe2d05613f51d5105673a0dc05871f780c4
Willian <willianyos13@gmail.com>
2022-05-27 10:02:19 -0300

    Created Superclass exercicio 03

---
+++ aulas/java/2022-05-25-ExerciciosSuperClasse/src/Exercicio03/FuncionarioTecnico.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio03;
+
+/**
+ *
+ * @author Willian
+ */
+public class FuncionarioTecnico extends Departamento {
+
+    private String Codigo;
+    private String Nome;
+    private float Salario;
+
+    public FuncionarioTecnico(String Codigo, String Nome, float Salario) {
+        this.Codigo = Codigo;
+        this.Nome = Nome;
+        this.Salario = Salario;
+    }
+}
---
+++ aulas/java/2022-05-25-ExerciciosSuperClasse/src/Exercicio03/Universidade.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio03;
+
+/**
+ *
+ * @author Willian
+ */
+public class Universidade {
+
+    protected String NomeUniversidade;
+
+    public Universidade(String NomeUniversidade) {
+        this.NomeUniversidade = NomeUniversidade;
+    }
+}
