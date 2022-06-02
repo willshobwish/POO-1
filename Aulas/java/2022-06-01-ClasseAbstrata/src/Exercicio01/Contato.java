7d51c37eb95b46216b116887451e39653e743906
Willian <willianyos13@gmail.com>
2022-06-02 09:14:24 -0300

    Created abstract class lesson

---
+++ Aulas/java/2022-06-01-ClasseAbstrata/src/Exercicio01/Contato.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio01;
+
+/**
+ *
+ * @author Willian
+ */
+public abstract class Contato {
+
+    protected String Tipo;
+
+    public Contato(String Contato) {
+
+    }
+
+    public abstract void Exibir();
+}
