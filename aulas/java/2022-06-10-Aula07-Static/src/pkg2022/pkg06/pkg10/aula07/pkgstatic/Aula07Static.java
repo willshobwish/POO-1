e8b0dea569da7a456a64221402fdde04922bcefb
Willian <willianyos13@gmail.com>
2022-06-22 09:41:41 -0300

    Created aula 07 exercicio 01

---
+++ aulas/java/2022-06-10-Aula07-Static/src/pkg2022/pkg06/pkg10/aula07/pkgstatic/Aula07Static.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2022.pkg06.pkg10.aula07.pkgstatic;
+
+/**
+ *
+ * @author Willian
+ */
+public class Aula07Static {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Math.PI);
+    }
+
+}
